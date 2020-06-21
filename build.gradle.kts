45792c6746cc87414a6a809efa9ac6fe25889129
Jonas Waage <jonas.h.waage@gmail.com>
2020-06-21 23:50:35 +0200

    Add license and pom generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
     id("maven-publish")
 }
 
-
-
 group = "com.bitbreeds.crypto"
 version = "1.0-SNAPSHOT"
 
@@ -32,6 +30,28 @@ publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["kotlin"])
+
+            pom {
+                name.set(rootProject.name)
+                description.set("Kotlin definitions for sodium")
+                url.set("https://github.com/IIlllII/sodium-kt-ext")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Jonas")
+                        name.set("Jonas Waage")
+                        email.set("jonas.waage@bitbreeds.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/IIlllII/sodium-kt-ext.git")
+                }
+            }
         }
     }
 }
